d54cf8067ab03f956a9849b7993f5a6c87e0893e
zae-park <tom941105@gmail.com>
2025-05-05 17:13:29 +0900

    feat: update structure

--- leetcode_sync.py
+++ leetcode_sync.py
@@ -12,10 +12,14 @@ if not LEETCODE_SESSION or not CSRF_TOKEN:
 
 # `leetcode-export` 명령어 실행 예시 (자동화된 풀이 코드 가져오기)
 def sync_leetcode_problems():
-    # Python, SQL, Pandas 문제만 가져오기
-    os.system(f"leetcode-export --cookies 'csrftoken={CSRF_TOKEN};LEETCODE_SESSION={LEETCODE_SESSION}' --only-accepted --only-last-submission --language=python --problem-folder-name 'root/easy/algorithm'")
-    os.system(f"leetcode-export --cookies 'csrftoken={CSRF_TOKEN};LEETCODE_SESSION={LEETCODE_SESSION}' --only-accepted --only-last-submission --language=mysql --problem-folder-name 'root/easy/database/sql'")
-    os.system(f"leetcode-export --cookies 'csrftoken={CSRF_TOKEN};LEETCODE_SESSION={LEETCODE_SESSION}' --only-accepted --only-last-submission --language=pandas --problem-folder-name 'root/easy/pandas'")
+    # Pandas 문제는 pythondata로 처리 (pythondata는 pandas로 풀이한 문제)
+    os.system(
+        f"leetcode-export --cookies 'csrftoken={CSRF_TOKEN};LEETCODE_SESSION={LEETCODE_SESSION}' --only-accepted --only-last-submission --language=pythondata --problem-folder-name 'root/easy/pandas'")
+
+    # SQL 문제는 mysql로 처리 (mysql은 database로 분류)
+    os.system(
+        f"leetcode-export --cookies 'csrftoken={CSRF_TOKEN};LEETCODE_SESSION={LEETCODE_SESSION}' --only-accepted --only-last-submission --language=mysql --problem-folder-name 'root/easy/database/sql'")
+
 
 # 디렉토리 구조 생성 함수
 def create_directory_structure(category, subcategory, difficulty, title_slug, problem_id, problem_name):
@@ -33,7 +37,8 @@ def save_problem_description_and_solution(problem_name, title_slug, category, su
     문제 설명과 해결책을 해당 디렉토리 구조로 저장
     """
     # 문제 디렉토리 생성
-    directory_name = create_directory_structure(category, subcategory, difficulty, title_slug, problem_name, problem_name)
+    directory_name = create_directory_structure(category, subcategory, difficulty, title_slug, problem_name,
+                                                problem_name)
 
     # 문제 설명 저장 (description.md)
     problem_description_url = f'https://leetcode.com/problems/{title_slug}/description/'
@@ -48,7 +53,8 @@ def save_problem_description_and_solution(problem_name, title_slug, category, su
     solution_path = os.path.join(directory_name, f'solution.{category}')
 
     # 여기서 사용자가 푼 풀이 코드를 가져와서 solution 파일로 저장
-    os.system(f"leetcode-export --cookies 'csrftoken={CSRF_TOKEN};LEETCODE_SESSION={LEETCODE_SESSION}' --only-accepted --only-last-submission --language={category} --problem-folder-name {directory_name}")
+    os.system(
+        f"leetcode-export --cookies 'csrftoken={CSRF_TOKEN};LEETCODE_SESSION={LEETCODE_SESSION}' --only-accepted --only-last-submission --language={category} --problem-folder-name {directory_name}")
 
     print(f'Problem {problem_name} synchronized in {directory_name}')
 
