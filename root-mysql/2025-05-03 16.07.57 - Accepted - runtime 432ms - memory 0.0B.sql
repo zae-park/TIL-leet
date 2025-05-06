5d4b5b44a5956fc604810285f2f24a59f400ea2b
zae-park <tom941105@gmail.com>
2025-05-07 00:43:07 +0900

    remove dummy files

--- root-mysql/2025-05-03 16.07.57 - Accepted - runtime 432ms - memory 0.0B.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT customer_number
-FROM Orders
-GROUP BY customer_number
-ORDER BY COUNT(*) DESC
-LIMIT 1;
--- root/pythondata/2025-05-01 09.33.29 - Accepted - runtime 287ms - memory 67.1MB.pd.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    return person[person.duplicated('email', keep=False)][['email']].drop_duplicates().rename(columns={'email': 'Email'})
